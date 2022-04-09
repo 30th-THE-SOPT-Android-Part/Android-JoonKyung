230c34650add6c1e2c7f5c11603638f78bb2d733
lee989898 <wnsrud1352@gmail.com>
2022-04-09 09:07:25 +0900

    feat: week1 실습 2

---
+++ app/src/main/java/com/lee989898/soptlee/EmptyActivity.kt
@@ -0,0 +1,11 @@
+package com.lee989898.soptlee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmptyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_empty)
+    }
+}
--- app/src/main/java/com/lee989898/soptlee/MainActivity.kt
+++ app/src/main/java/com/lee989898/soptlee/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.lee989898.soptlee
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.lee989898.soptlee.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mainLoginBt.setOnClickListener {
+            val intent = Intent(this, EmptyActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
