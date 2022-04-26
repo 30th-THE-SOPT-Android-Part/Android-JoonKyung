9d84847d9679ebd5ead7ebfa00e141b6d5d6e3a2
lee989898 <wnsrud1352@gmail.com>
2022-04-26 23:38:07 +0900

    feat: 프로필 프래그먼트 추가

---
+++ app/src/main/java/com/lee989898/soptlee/ProfileFragment.kt
@@ -0,0 +1,30 @@
+package com.lee989898.soptlee
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.lee989898.soptlee.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = DataBindingUtil.inflate(inflater,R.layout.fragment_profile, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
