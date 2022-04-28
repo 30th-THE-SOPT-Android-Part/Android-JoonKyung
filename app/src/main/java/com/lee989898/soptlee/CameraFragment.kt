2a6df6e0a947a359e88ace61d63ea75be59c72e2
lee989898 <wnsrud1352@gmail.com>
2022-04-28 22:00:39 +0900

    feat: 심화과제

--- app/src/main/java/com/lee989898/soptlee/CameraFragment.kt
+++ app/src/main/java/com/lee989898/soptlee/CameraFragment.kt
@@ -1,25 +1,59 @@
 package com.lee989898.soptlee
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.lee989898.soptlee.R
 import com.lee989898.soptlee.databinding.FragmentCameraBinding
+import java.lang.Exception
 
 class CameraFragment : Fragment() {
 
-    private var _binding : FragmentCameraBinding? = null
+    private var _binding: FragmentCameraBinding? = null
     private val binding get() = _binding!!
+    private val REQUEST_CODE = 10
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = DataBindingUtil.inflate(inflater,R.layout.fragment_camera, container, false)
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_camera, container, false)
+
+        binding.cameraInsertBt.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = MediaStore.Images.Media.CONTENT_TYPE
+
+            startActivityForResult(intent, REQUEST_CODE)
+
+        }
+
         return binding.root
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                try {
+                    val uri = data?.data
+                    Glide.with(this)
+                        .load(uri)
+                        .into(binding.cameraImgIv)
+                } catch (e: Exception) {
+
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+
+            }
+        }
     }
 
     override fun onDestroyView() {
